8d1ecdd252161f7d29aa1bbc59f6fbf537c4d0d5
Unknown <avaearthben2@gmail.com>
2017-04-20 16:52:04 -0400

    Added Audio Script to Dog (Walking and Panting so far)

---
+++ Assets/Scripts/Audio Scripts/DogSounds.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Collections;
+
+public class DogSounds : MonoBehaviour {
+
+	private float time = 5.0f;
+	public AudioClip dogPanting;
+	public AudioClip dogWalking;
+	AudioSource dogWalkingSound;
+	AudioSource dogPantingSound;
+	// Use this for initialization
+	void Start () {
+		AudioSource[] audios = GetComponents<AudioSource>();
+		dogWalkingSound = audios[0];
+		dogPantingSound = audios[1];
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		time -= Time.deltaTime;
+		Debug.Log (time);
+		if (Input.GetKey (KeyCode.A) || Input.GetKey (KeyCode.W) || Input.GetKey (KeyCode.D) || Input.GetKey (KeyCode.S)) { //if your are moving
+			if (dogWalkingSound.isPlaying == false) {
+				dogWalkingSound.Play (); //play walking sound
+			} 
+		} else {
+			dogWalkingSound.Stop (); //stop playing the sound if you are not pressing anything
+		}
+
+		if (time <= 0.0f) {
+			if (dogPantingSound.isPlaying == false) {
+				dogPantingSound.Play ();
+			}
+		} else {
+			dogPantingSound.Stop ();
+		}
+
+		if (time <= -7.0f) {
+			time = 20.0f;
+		}
+	}		
+}
