8e60cb3b6a56c1e3c2937ff59ad97bb6fc5ed66c
Easton <eastonself@gmail.com>
2017-04-26 23:46:07 -0400

    I fixed the game
    Let's get this done

--- Assets/cameraStateChange.cs
+++ Assets/cameraStateChange.cs
@@ -3,11 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class cameraStateChange : MonoBehaviour {
-
-	// Use this for initialization
-	void Start () {
-		
-	}
+	
 	
 	// Update is called once per frame
 	void Update () {
